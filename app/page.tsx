e70f2d0c8f8f7f5eac5f621213e1025cba63f154
Bhavankumar Ganesan <bhavankumarganesan@IVMBA-M2-Bhavan-Laptop--22.local>
2024-10-15 10:19:05 +0530

    Contact

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Banner from "../components/HomePage/Banner/Banner";
 import WhatWe from "../components/HomePage/WhatWe/WhatWe";
 import OurPresence from "../components/HomePage/OurPresense/OurPresence";
 import Product from "../components/HomePage/ProductRage/Product";
-import Contact from "../components/HomePage/Contact/Contact";
-
+// import Contact from "../components/HomePage/Contact/Contact";
+import Contact from "@/components/ContactUs/Form/Page";
 export default function Home() {
   return (
     <>
