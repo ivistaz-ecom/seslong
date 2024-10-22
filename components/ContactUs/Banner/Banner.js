142eaf13a9c6634457249e4a608daaa1250f64cf
Bhavankumarg <gbhavanbkr@gmail.com>
2024-10-22 14:45:24 +0530

    banner mobile

--- components/ContactUs/Banner/Banner.js
+++ components/ContactUs/Banner/Banner.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Banner() {
   return (
     <div className="z-50 h-[50vh]">
-      <div className="lg:h-[50vh] h-[20vh] bg-[#F8E2CF]">
+      <div className="lg:h-[50vh] h-[30vh] bg-[#F8E2CF]">
         <div className="relative h-full w-full overflow-hidden">
           <div
             className={`absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 transition-opacity duration-700 ease-in-out `}
           >
             <img
               src="/contact-banner.png"
-              className="lg:size-full h-36 object-cover"
+              className="lg:size-full h-52 object-cover"
               alt="Slide image"
             />
           </div>
