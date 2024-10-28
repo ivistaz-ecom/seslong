08a827e11f3a8f945ca08fdea45f855399a57b25
Bhavankumarg <gbhavanbkr@gmail.com>
2024-10-28 16:30:58 +0530

    floating changes

--- components/WhatsAppFloatingButton/page.js
+++ components/WhatsAppFloatingButton/page.js
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from "react";
 import { IoMdMail } from "react-icons/io"; // This import is not used, consider removing it if unnecessary
 import { useRouter } from "next/navigation"; // If you're not using `router` for navigation, you can also remove this import
 import { BsWhatsapp } from "react-icons/bs";
+import Link from "next/link";
 
 export default function FloatingButton() {
     const [isTextVisible, setIsTextVisible] = useState(true);
 
     const handleClick = () => {
         // Open WhatsApp link in a new tab
-        window.open(`https://wa.me/9500642143?text=${encodeURIComponent("Hi, I have an enquiry!")}`, "_blank");
-
+        // window.open(`https://wa.me/9500642143?text=${encodeURIComponent("Hi, I have an enquiry!")}`, "_blank");
     };
 
     // Toggle text visibility based on scroll position
@@ -32,13 +32,14 @@ export default function FloatingButton() {
 
     return (
         <div className="fixed bottom-24 right-4 z-50 flex items-center space-x-2 lg:right-8">
-            <button
-                onClick={handleClick}
-                className="flex items-center justify-center rounded-full  p-3 text-2xl bg-green-500 text-white shadow-lg transition duration-300 ease-in-out"
-            >
-                <BsWhatsapp size={28} />
-            </button>
-
+            <Link href="https://wa.me/9500642143?text=Hi, I have an enquiry!" target="_blank">
+                <button
+                    // onClick={handleClick}
+                    className="flex items-center justify-center rounded-full  bg-green-500 p-3 text-2xl text-white shadow-lg transition duration-300 ease-in-out"
+                >
+                    <BsWhatsapp size={28} />
+                </button>
+            </Link>
         </div>
     );
 }
