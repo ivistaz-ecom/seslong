12baae280485de76a65f93f0af7f4644e9b93459
Bhavankumarg <gbhavanbkr@gmail.com>
2024-12-13 20:08:39 +0530

    banner text center home

--- components/HomePage/Banner/Banner.js
+++ components/HomePage/Banner/Banner.js
@@ -40,75 +40,11 @@ export default function Banner() {
   );
 
   return (
-    <div className="z-50">
-      {/* <div className="h-[70vh] bg-[#F8E2CF]">
-        <div className="relative h-full w-full overflow-hidden">
-          {homeSlider.map((slide, index) => (
-            <div
-              key={index}
-              className={`absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 transition-opacity duration-700 ease-in-out ${
-                index === activeIndex ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              <img
-                src={slide.image}
-                className="hidden h-full w-full object-cover sm:block"
-                alt={slide.alt || "Slide image"}
-              />
-              <img
-                src={slide.mobImage}
-                className="block h-full w-full object-cover sm:hidden"
-                alt={slide.alt || "Slide image"}
-              />
-              <div
-                className="absolute bottom-28 left-4 bg-black bg-opacity-50 p-2 py-2 text-2xl font-medium text-white lg:bottom-32 lg:left-10 lg:px-4 lg:text-5xl"
-                dangerouslySetInnerHTML={{ __html: slide.text }}
-              ></div>
-            </div>
-          ))}
-
-          <div className="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse">
-            {homeSlider.map((_, index) => (
-              <button
-                key={index}
-                type="button"
-                className={`h-1 rounded-lg transition-all duration-300 ease-in-out ${
-                  index === activeIndex ? "w-40" : "w-20"
-                } bg-white`}
-                aria-current={index === activeIndex ? "true" : "false"}
-                onClick={() => setActiveIndex(index)}
-              ></button>
-            ))}
-          </div>
-
-          {prevBtn}
-          {nextBtn}
-        </div>
-      </div> */}
-      <div className="z-50 ">
-        <div className="h-[70vh] bg-[#F8E2CF] lg:h-[50vh]">
-          <div className="relative size-full overflow-hidden">
-            <div
-              className={`absolute left-1/2 top-1/2 size-full -translate-x-1/2 -translate-y-1/2 transition-opacity duration-700 ease-in-out`}
-            >
-              {/* Desktop image */}
-              <Image
-                width={1200}
-                height={900}
-                src="/home-page-banner.png"
-                className="hidden size-full object-cover sm:block"
-                alt="Desktop banner"
-              />
-              {/* Mobile image */}
-              <Image
-                height={500}
-                width={600}
-                src="/home-page-banner-mobile.png"
-                className="block size-full object-cover sm:hidden"
-                alt="Mobile banner"
-              />
-            </div>
-            <div className="absolute bottom-28  left-4 bg-black bg-opacity-65 p-2 py-2 text-2xl font-medium text-white lg:bottom-12 lg:left-16 lg:px-4 lg:text-5xl">
+    <div className="">
+      <div className="">
+        <div className="flex h-[50vh] w-screen bg-[url('/home-page-banner-mobile.png')] bg-cover bg-center bg-no-repeat lg:bg-[url('/home-page-banner.png')]">
+          <div className="container mx-auto flex items-center justify-center p-5 text-center text-3xl text-white lg:text-5xl">
+            <div className="inline-block bg-black bg-opacity-65 px-6 py-4">
               <p>
                 Your Reliable Partner For <br /> Quality Products
               </p>
@@ -116,8 +52,9 @@ export default function Banner() {
           </div>
         </div>
       </div>
+
       <div className="z-40 mx-auto flex items-center justify-center bg-[#F8E2CF] p-5 text-xl font-normal lg:h-[20vh] lg:px-40">
-        <p className="lg:w-10/12w container mx-auto">
+        <p className="container mx-auto lg:w-10/12">
           Seslong is a global trading house with operations and manufacturing
           strategically located across India, Hong Kong, China, enabling us to
           supply a diverse range of products to customers worldwide.
